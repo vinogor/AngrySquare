6d07bbc7a820ab455ce27497c37e9c775d25e66e
Aleksandr Andreev <andreev.aleksandr.spb@gmail.com>
2024-02-09 12:15:42 +0300

    BaseSettings

---
+++ Assets/Sсripts/Scriptable/BaseSettings.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Sсripts.Scriptable
+{
+    [CreateAssetMenu(fileName = "BaseSettings", menuName = "Gameplay/BaseSettings")]
+    public class BaseSettings : ScriptableObject
+    {
+        // TODO: ??? 
+    }
+}
